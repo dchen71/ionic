c46ca0625720041b1817a38b05751f78a612f903
Dan <dchen12812@gmail.com>
2016-02-17 20:11:27 -0800

    added favorite filter

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -245,4 +245,16 @@ angular.module('conFusion.controllers', [])
             $scope.shouldShowDelete = false;
 
         }}])
+    .filter('favoriteFilter', function () {
+        return function (dishes, favorites) {
+            var out = [];
+            for (var i = 0; i < favorites.length; i++) {
+                for (var j = 0; j < dishes.length; j++) {
+                    if (dishes[j].id === favorites[i].id)
+                        out.push(dishes[j]);
+                }
+            }
+            return out;
+
+        }});
 
