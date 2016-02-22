a3cb605db076dc36e18d6721e603395706c59b8b
Dan <dchen12812@gmail.com>
2016-02-21 23:41:09 -0800

    updated favorite controller

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -223,16 +223,25 @@ angular.module('conFusion.controllers', [])
         $scope.baseURL = baseURL;
         $scope.shouldShowDelete = false;
 
+        $ionicLoading.show({
+            template: '<ion-spinner></ion-spinner> Loading...'
+        });
+
         $scope.favorites = favoriteFactory.getFavorites();
 
         $scope.dishes = menuFactory.getDishes().query(
             function (response) {
                 $scope.dishes = response;
+                $timeout(function () {
+                    $ionicLoading.hide();
+                }, 1000);
             },
             function (response) {
                 $scope.message = "Error: " + response.status + " " + response.statusText;
+                $timeout(function () {
+                    $ionicLoading.hide();
+                }, 1000);
             });
-        console.log($scope.dishes, $scope.favorites);
 
         $scope.toggleDelete = function () {
             $scope.shouldShowDelete = !$scope.shouldShowDelete;
