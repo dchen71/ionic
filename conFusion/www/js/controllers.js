a34a44bcc11485b4f2175ed3456e593877d6a77e
Dan <dchen12812@gmail.com>
2016-02-27 22:31:30 -0800

    added localstorage to controller ref

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -2,7 +2,7 @@
 
 angular.module('conFusion.controllers', [])
 
-    .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $localStorage) {
     
       // With the new view caching in Ionic, Controllers are only called
       // when they are recreated or on app start, instead of every page change.
