414117dbae8355cc2b7ebefce6ea29d4f0abe7b9
Dan <dchen12812@gmail.com>
2016-02-27 22:32:33 -0800

    updated do login function

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -32,8 +32,9 @@ angular.module('conFusion.controllers', [])
       };
     
       // Perform the login action when the user submits the login form
-      $scope.doLogin = function() {
+      $scope.doLogin = function () {
         console.log('Doing login', $scope.loginData);
+        $localStorage.storeObject('userinfo',$scope.loginData);
     
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
