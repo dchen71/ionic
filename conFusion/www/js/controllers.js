bafffb8c7cf9104bbf53a0641d0b7e982ae7b04a
Dan <dchen12812@gmail.com>
2016-03-13 20:05:45 -0700

    add favorite modified to notificiations

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -73,11 +73,28 @@ angular.module('conFusion.controllers', [])
     })
 
     .controller('MenuController', ['$scope', 'dishes', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPlatform', '$cordovaLocalNotification', '$cordovaToast', function ($scope, dishes, favoriteFactory, baseURL, $ionicListDelegate, $ionicPlatform, $cordovaLocalNotification, $cordovaToast) {
-            
+
         $scope.addFavorite = function (index) {
-            console.log("index is " + index);
-            favoriteFactory.addToFavorites(index);
-            $ionicListDelegate.closeOptionButtons();
+            $ionicPlatform.ready(function () {
+                    $cordovaLocalNotification.schedule({
+                        id: 1,
+                        title: "Added Favorite",
+                        text: $scope.dishes[index].name
+                    }).then(function () {
+                        console.log('Added Favorite '+$scope.dishes[index].name);
+                    },
+                    function () {
+                        console.log('Failed to add Notification ');
+                    });
+
+                    $cordovaToast
+                      .show('Added Favorite '+$scope.dishes[index].name, 'long', 'center')
+                      .then(function (success) {
+                          // success
+                      }, function (error) {
+                          // error
+                      });
+            });
         }
         
         $scope.baseURL = baseURL;    
